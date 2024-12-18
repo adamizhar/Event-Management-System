6c7b837e933846d38fb5a6791ef46d143ffdb0d8
adamizhar <adamhaikal588@gmail.com>
2024-12-18 16:56:14 +0800

    Adding AE, CE, E and main file.java

---
+++ Event.java
@@ -0,0 +1,25 @@
+public class Event { 
+    protected int eventID;
+    protected String eventName;
+    protected String eventDate;
+
+    public Event(int eventID, String eventName, String eventDate){
+        this.eventID = eventID;
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+    }
+
+    public void getEventID(){
+        System.out.println("Please Enter event id: ");
+        
+
+    }
+
+    public void getEventName(){
+
+    }
+
+    public void getEventDate(){
+
+    }
+}
